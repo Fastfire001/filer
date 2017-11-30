c608aabb585eaa272dc092a07c8841dc62cdab8a
Maxime Maréchal <maxime.marechal@supinternet.fr>
2017-11-30 16:48:53 +0100

    Added layout.php

---
+++ layout.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title><?php echo $title; ?></title>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet"> <!--font -->
+    <link rel="stylesheet" href="./assets/styles.css" type="text/css" />
+</head>
+<body>
+    <header>
+        <div>My Filer</div>
+    </header>
+    <main>
+        <?php echo $content; ?>
+    </main>
+    <footer>
+        Maxime Maréchal &#169;
+    </footer>
+</body>
+</html>
