5c81a71a575b78e7a69f5e3e4b8f5739a7f55a3f
Maxime Maréchal <maxime.marechal@supinternet.fr>
2017-12-31 12:45:58 +0100

    Added form verification and insert in db

--- addFile.php
+++ addFile.php
@@ -1,3 +1,18 @@
 <?php
+require ('init.php');
 $fileName =  $_POST['fileName'];
 $file = $_POST['file'];
+$userID = intval($_SESSION['id']);
+$isFileValid = false;
+if (!empty($fileName) && 0 !== strlen($fileName) && !empty($file)){
+    $isFileValid = true;
+}
+if (true === $isFileValid){
+    $path = "./" . $userID . "/" . $fileName;
+    $q = "INSERT INTO `files` (`file_id`, `user_id`, `name`, `path`) VALUES (NULL, ?, ?, ?)";
+    $stmt = mysqli_prepare($link, $q);
+    mysqli_stmt_bind_param($stmt, 'iss', $userID, $fileName, $path);
+    mysqli_stmt_execute($stmt);
+}
+header('Location: index.php');
+exit();
