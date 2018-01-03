4aa0181698081ee8a4cde2b95007b847e2c1e6ef
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-03 16:43:37 +0100

    Added upload file functionnality

--- addFile.php
+++ addFile.php
@@ -1,18 +1,32 @@
 <?php
 require ('init.php');
 $fileName =  $_POST['fileName'];
-$file = $_POST['file'];
-$userID = intval($_SESSION['id']);
+$file = $_FILES['file'];
+$userID = $_SESSION['id'];
+$path = "./files/" . $userID . "/" . $fileName;
 $isFileValid = false;
 if (!empty($fileName) && 0 !== strlen($fileName) && !empty($file)){
     $isFileValid = true;
 }
 if (true === $isFileValid){
-    $path = "./" . $userID . "/" . $fileName;
-    $q = "INSERT INTO `files` (`file_id`, `user_id`, `name`, `path`) VALUES (NULL, ?, ?, ?)";
-    $stmt = mysqli_prepare($link, $q);
-    mysqli_stmt_bind_param($stmt, 'iss', $userID, $fileName, $path);
-    mysqli_stmt_execute($stmt);
+    if (!file_exists('./files/' . $userID)) {
+        mkdir('./files/' . $userID, 0777);
+    }
+    if (!file_exists('./files/' . $userID . '/' . $file['name'])){
+
+        $uploaddir = './files/' . $userID . '/';
+        $uploadfile = $uploaddir . basename($_FILES['file']['name']);
+        $tmp_file = $file['tmp_name'];
+        move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
+
+
+        $q = "INSERT INTO `files` (`file_id`, `user_id`, `name`, `path`) VALUES (NULL, ?, ?, ?)";
+        $stmt = mysqli_prepare($link, $q);
+        $userID = intval($userID);
+        mysqli_stmt_bind_param($stmt, 'iss', $userID, $fileName, $path);
+        mysqli_stmt_execute($stmt);
+    }
+
 }
 header('Location: index.php');
 exit();
