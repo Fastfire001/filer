9e5763e60f9a9da175a998917df5a5ecea11e4e0
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-09 19:26:24 +0100

    Added script for popup and form verification

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    var btnrename = document.querySelectorAll('.rename');
+    for (var i = 0; i < btnrename.length; i++){
+        btnrename[i].onclick = function () {
+            var oldName = this.previousSibling.previousSibling.previousSibling.previousSibling.textContent;
+            document.querySelector('div.popup').classList.remove("hide");
+            document.querySelector('div.overlay').classList.remove("hide");
+            document.querySelector('input#newName').value = oldName;
+        };
+    }
+    document.querySelector('form.renameForm').onsubmit = function () {
+        var newName = this.elements['newName'].value;
+        if (0 === newName.length){
+            document.querySelector('div.popup').classList.add("hide");
+            document.querySelector('div.overlay').classList.add("hide");
+            alert("Invalid name");
+            return false;
+        } else {
+            return true;
+        }
+    };
+};
