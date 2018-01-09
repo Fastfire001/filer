a4e78b6b861c1b879d024a9f2d9d65081fef8b3d
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-09 19:11:17 +0100

    patched file_exists at line 15

--- addFile.php
+++ addFile.php
@@ -12,20 +12,19 @@ if (true === $isFileValid){
     if (!file_exists('./files/' . $userID)) {
         mkdir('./files/' . $userID, 0777);
     }
-    if (!file_exists('./files/' . $userID . '/' . $file['name'])){
+    if (!file_exists('./files/' . $userID . '/' . $fileName)){
 
         $uploaddir = './files/' . $userID . '/';
         $uploadfile = $uploaddir . basename($_FILES['file']['name']);
         move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
         rename($uploadfile, $uploaddir . $fileName);
-
-
         $q = "INSERT INTO `files` (`file_id`, `user_id`, `name`, `path`) VALUES (NULL, ?, ?, ?)";
         $stmt = mysqli_prepare($link, $q);
         $userID = intval($userID);
         mysqli_stmt_bind_param($stmt, 'iss', $userID, $fileName, $path);
         mysqli_stmt_execute($stmt);
     }
+    var_dump(basename($_FILES['file']['name']));
 
 }
 header('Location: index.php');
