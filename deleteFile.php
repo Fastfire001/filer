499152126c54b9d6a1afd3caeeee626074ffa3a4
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-10 12:04:40 +0100

    removed $

--- deleteFile.php
+++ deleteFile.php
@@ -3,7 +3,7 @@ require ('init.php');
 $fileID = $_GET['id'];
 $name = $_GET['name'];
 $userID = $_SESSION['id'];
-$mysqli_query($link, 'DELETE FROM `files` WHERE user_id =' . $userID . ' AND file_id =' . $fileID);
+mysqli_query($link, 'DELETE FROM `files` WHERE user_id =' . $userID . ' AND file_id =' . $fileID);
 unlink('./files/' . $userID . '/' . $name);
 header('Location: index.php');
 exit();
