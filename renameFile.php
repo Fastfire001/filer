2affe45b7e886cc1b4ddfafabe7e5bed4ed4e22f
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-10 12:09:58 +0100

    Added rename functionnality

--- renameFile.php
+++ renameFile.php
@@ -1,1 +1,13 @@
 <?php
+require ('init.php');
+$userID = $_SESSION['id'];
+$newName = htmlentities($_POST['newName']);
+$oldName = $_POST['oldFileName'];
+$fileID = $_POST['fileID'];
+$newPath = "./files/" . $userID . "/" . $newName;
+$oldPath = "./files/" . $userID . "/" . $oldName;
+//UPDATE files SET name = 'newFakeName', path = './files/2/newFakeName' WHERE file_id = 73
+mysqli_query($link,"UPDATE files SET name = '" . $newName . "' , path = '" . $newPath . "' WHERE file_id = " . $fileID);
+rename($oldPath, $newPath);
+header('Location: index.php');
+exit();
