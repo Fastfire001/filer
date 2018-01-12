ee56ea12d8d7059e2b2a352997aa46fa3826b8cb
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-12 13:58:51 +0100

    Added maj for error message

--- renameFile.php
+++ renameFile.php
@@ -6,7 +6,10 @@ $oldName = $_POST['oldFileName'];
 $fileID = $_POST['fileID'];
 $newPath = "./files/" . $userID . "/" . $newName;
 $oldPath = "./files/" . $userID . "/" . $oldName;
-//UPDATE files SET name = 'newFakeName', path = './files/2/newFakeName' WHERE file_id = 73
+if (file_exists($newPath)){
+    header('Location: index.php?error=File already exist');
+    exit();
+}
 mysqli_query($link,"UPDATE files SET name = '" . $newName . "' , path = '" . $newPath . "' WHERE file_id = " . $fileID);
 rename($oldPath, $newPath);
 header('Location: index.php');
